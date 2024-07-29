51ebfdf10fb5a07834e9784685304648c4a714a1
Deema35 <deema_35@mail.ru>
2024-07-30 00:01:14 +0300

    Add some impruvments.

--- stm32_keyboard/src/main.c
+++ stm32_keyboard/src/main.c
@@ -2,21 +2,21 @@
 
 /* USER CODE BEGIN Header */
 /**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2024 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
+	******************************************************************************
+	* @file           : main.c
+	* @brief          : Main program body
+	******************************************************************************
+	* @attention
+	*
+	* Copyright (c) 2024 STMicroelectronics.
+	* All rights reserved.
+	*
+	* This software is licensed under terms that can be found in the LICENSE file
+	* in the root directory of this software component.
+	* If no LICENSE file comes with this software, it is provided AS-IS.
+	*
+	******************************************************************************
+	*/
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
@@ -56,16 +56,16 @@ A\B |  0  |  1  |  2  |  3  |  4  |  5  |  6  |  7  |  8  |  9  |  10  |  11  | 
 
 typedef struct
 {
-  int Port;
-  uint16_t PinNum;
-  uint8_t KEYCODE1;
-  uint8_t MODIFIER;
+	int Port;
+	uint16_t PinNum;
+	uint8_t KEYCODE1;
+	uint8_t MODIFIER;
 } KeyDesk;
 
 enum GPIO_PORT
 {
-  PORT_A,
-  PORT_B
+	PORT_A,
+	PORT_B
 };
 
 KeyDesk Key_F7 = {PORT_B, GPIO_PIN_0, 0x40, 0b00000000};
@@ -141,7 +141,7 @@ KeyDesk Key_4 = {PORT_B, GPIO_PIN_15, 0x21, 0b00000000};
 KeyDesk Key_F8 = {PORT_B, GPIO_PIN_0, 0x41, 0b00000000};
 KeyDesk Bacsp = {PORT_B, GPIO_PIN_1, 0x2a, 0b00000000};
 KeyDesk Key_quotes = {PORT_B, GPIO_PIN_3, 0x34, 0b00000000};
-KeyDesk Rbracket = {PORT_B, GPIO_PIN_6, 0x30, 0b00000000};
+KeyDesk Rbracket = {PORT_B, GPIO_PIN_6, 0x30, 0b00000000}; 
 KeyDesk Key_O = {PORT_B, GPIO_PIN_7, 0x12, 0b00000000};
 KeyDesk Key_U = {PORT_B, GPIO_PIN_8, 0x18, 0b00000000};
 KeyDesk Key_T = {PORT_B, GPIO_PIN_9, 0x17, 0b00000000};
@@ -161,14 +161,14 @@ KeyDesk Key_5 = {PORT_B, GPIO_PIN_15, 0x22, 0b00000000};
 
 KeyDesk* KeyRows[8][12] = 
 {
-  {&Key_F7, &Pause, &LCtrl, &Menu, &Key_M, &Key_dash, &Space, &Key_F6, &Left, &LAlt, &Win, NULL},
-  {&Key_F3, &Up, &Key_Dot, &RCtrl, &LShif, &Key_K, &Key_N, &Key_V, &Key_F2, &Right, &Key_D, &Key_Z},
-  {&Key_1, &Prtsc, &Comma, &Key_B, &Key_C, &Capslk, &Down, &RAlt, &Key_X, NULL, NULL, NULL},
-  {&Key_F4, &Key_2, &Key_Slash, &RShif, &Key_L, &Key_J, &Key_G, &Key_F1, &PgDw, &Key_F, &Key_A, NULL},
-  {&Key_Q, &Key_3, &Key_semicolon, &Enter, &Key_I, &Key_Y, &Key_H, &Key_Tab, &PgUp, &Key_E, &Key_S, NULL, NULL},
-  {&Key_F12, &Key_F9, &Key_P, &Key_equals, &Key_0, &Key_8, &Key_6, &Key_tilde, &Key_del, &Key_4, NULL, NULL},
-  {&Key_F8, &Bacsp, &Key_quotes, &Rbracket, &Key_O, &Key_U, &Key_T, &Key_F5, &Key_R, &Key_W, NULL, NULL},
-  {&Key_F11, &Key_F10, &Lbracket, &Key_Minus, &Key_9, &Key_7, &Key_Esc, &Key_Ins, &Key_5, NULL, NULL, NULL}
+	{&Key_F7, &Pause, &LCtrl, &Menu, &Key_M, &Key_dash, &Space, &Key_F6, &Left, &LAlt, &Win, NULL},
+	{&Key_F3, &Up, &Key_Dot, &RCtrl, &LShif, &Key_K, &Key_N, &Key_V, &Key_F2, &Right, &Key_D, &Key_Z},
+	{&Key_1, &Prtsc, &Comma, &Key_B, &Key_C, &Capslk, &Down, &RAlt, &Key_X, NULL, NULL, NULL},
+	{&Key_F4, &Key_2, &Key_Slash, &RShif, &Key_L, &Key_J, &Key_G, &Key_F1, &PgDw, &Key_F, &Key_A, NULL},
+	{&Key_Q, &Key_3, &Key_semicolon, &Enter, &Key_I, &Key_Y, &Key_H, &Key_Tab, &PgUp, &Key_E, &Key_S, NULL},
+	{&Key_F12, &Key_F9, &Key_P, &Key_equals, &Key_0, &Key_8, &Key_6, &Key_tilde, &Key_del, &Key_4, NULL, NULL},
+	{&Key_F8, &Bacsp, &Key_quotes, &Rbracket, &Key_O, &Key_U, &Key_T, &Key_F5, &Key_R, &Key_W, NULL, NULL},
+	{&Key_F11, &Key_F10, &Lbracket, &Key_Minus, &Key_9, &Key_7, &Key_Esc, &Key_Ins, &Key_5, NULL, NULL, NULL}
 };
 
 uint8_t IsKeyPressed = 0;
@@ -178,226 +178,231 @@ uint32_t  TimmeFromPress = 0;
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 void KeyBoardPrint(char *data,uint16_t length);
+void AddKey(keyboardReportDes *Msg, u_int8_t KEYCODE, int num);
 
 /**
-  * @brief  The application entry point.
-  * @retval int
-  */
+	* @brief  The application entry point.
+	* @retval int
+	*/
 int main(void)
 {
  
 
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-  HAL_Init();
+	/* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+	HAL_Init();
 
-  /* Configure the system clock */
-  SystemClock_Config();
+	/* Configure the system clock */
+	SystemClock_Config();
 
-  /* Initialize all configured peripherals */
-  MX_GPIO_Init();
-  MX_USB_DEVICE_Init();
+	/* Initialize all configured peripherals */
+	MX_GPIO_Init();
+	MX_USB_DEVICE_Init();
 
-  /* Infinite loop */
-  
-  while (1)
-  {
+	/* Infinite loop */
+	
+	while (1)
+	{
 	 
-    keyboardReportDes HIDkeyBoardMessage = {0,0,0,0,0,0,0,0};
+		keyboardReportDes HIDkeyBoardMessage = {0,0,0,0,0,0,0,0};
 
-    uint8_t KeyNum = 0;
-    uint8_t KeyMod = 0;
-    GPIO_PinState CurentState;
-    
-    for (int i = 0; i < 8; i++)
-    {
-    
-      HAL_GPIO_WritePin(GPIOA, Ports[i], GPIO_PIN_RESET);
-      HAL_Delay(5);
-      for (int j = 0; j < 13; j++)
-      {
-        if (KeyRows[i][j])
-        {
-          if (KeyRows[i][j]->Port == PORT_A)
-          {
-            CurentState = HAL_GPIO_ReadPin(GPIOA, KeyRows[i][j]->PinNum);
-          }
-          else 
-          {
-            CurentState = HAL_GPIO_ReadPin(GPIOB, KeyRows[i][j]->PinNum);
-          }
-          
-          if (!CurentState)
-          {
-             if (!KeyNum && KeyRows[i][j]->KEYCODE1)
-            {
-              HIDkeyBoardMessage.KEYCODE1 = KeyRows[i][j]->KEYCODE1;
-              KeyNum = 1;
-            }
-            else if (KeyRows[i][j]->MODIFIER)
-            {
-              HIDkeyBoardMessage.MODIFIER |= KeyRows[i][j]->MODIFIER;
-              KeyMod = 1;
-            } 
-            
-          } 
-        }
-        
-        
-      }
-      
-      HAL_GPIO_WritePin(GPIOA, Ports[i], GPIO_PIN_SET);
-    } 
-   
-    if(KeyNum || KeyMod)
-    {
-      USBD_HID_SendReport(&hUsbDeviceFS,&HIDkeyBoardMessage,sizeof(HIDkeyBoardMessage));
-      IsKeyPressed = 1;
-      TimmeFromPress = HAL_GetTick();
-    }
-    else if (IsKeyPressed && ((HAL_GetTick() - TimmeFromPress) > 50))
-    {
-      
-      USBD_HID_SendReport(&hUsbDeviceFS,&HIDkeyBoardMessage,sizeof(HIDkeyBoardMessage));
-      IsKeyPressed = 0;
-      
-    }
-    
-  }
+		uint8_t KeyNum = 0;
+		uint8_t KeyMod = 0;
+		GPIO_PinState CurentState;
+
+		
+		
+		for (int i = 0; i < 8; i++)
+		{
+			HAL_GPIO_WritePin(GPIOA, Ports[i], GPIO_PIN_RESET);
+			HAL_Delay(5);
+			for (int j = 0; j < 12; j++)
+			{
+				if (KeyRows[i][j])
+				{
+					if (KeyRows[i][j]->Port == PORT_A)
+					{
+						CurentState = HAL_GPIO_ReadPin(GPIOA, KeyRows[i][j]->PinNum);
+					}
+					else 
+					{
+						CurentState = HAL_GPIO_ReadPin(GPIOB, KeyRows[i][j]->PinNum);
+					}
+					
+					if (!CurentState)
+					{
+						if (KeyRows[i][j]->KEYCODE1)
+						{
+							AddKey(&HIDkeyBoardMessage,KeyRows[i][j]->KEYCODE1,KeyNum);
+							KeyNum++;
+
+							
+						}
+						else if (KeyRows[i][j]->MODIFIER)
+						{
+							HIDkeyBoardMessage.MODIFIER |= KeyRows[i][j]->MODIFIER;
+							KeyMod = 1;
+						} 
+					
+					} 
+				}
+
+			}
+			HAL_GPIO_WritePin(GPIOA, Ports[i], GPIO_PIN_SET);
+		} 
+		
+		
+			
+		if(KeyNum || KeyMod)
+		{
+			USBD_HID_SendReport(&hUsbDeviceFS, (uint8_t *)&HIDkeyBoardMessage, sizeof(HIDkeyBoardMessage));
+			IsKeyPressed = 1;
+			TimmeFromPress = HAL_GetTick();
+				
+		}
+		else if (IsKeyPressed && ((HAL_GetTick() - TimmeFromPress) > 50))
+		{
+			
+			USBD_HID_SendReport(&hUsbDeviceFS, (uint8_t *)&HIDkeyBoardMessage, sizeof(HIDkeyBoardMessage));
+			IsKeyPressed = 0;
+			
+		}
+	
+	}
 }
 
 /**
-  * @brief System Clock Configuration
-  * @retval None
-  */
+	* @brief System Clock Configuration
+	* @retval None
+	*/
 void SystemClock_Config(void)
 {
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
-  RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
+	RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+	RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+	RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
 
-  /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
-  RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
-  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    Error_Handler();
-  }
+	/** Initializes the RCC Oscillators according to the specified parameters
+	* in the RCC_OscInitTypeDef structure.
+	*/
+	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+	RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+	RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
+	RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+	RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+	RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
+	if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+	{
+	Error_Handler();
+	}
 
-  /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+	/** Initializes the CPU, AHB and APB buses clocks
+	*/
+	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+								|RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USB;
-  PeriphClkInit.UsbClockSelection = RCC_USBCLKSOURCE_PLL_DIV1_5;
-  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
-  {
-    Error_Handler();
-  }
+	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+	{
+	Error_Handler();
+	}
+	PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USB;
+	PeriphClkInit.UsbClockSelection = RCC_USBCLKSOURCE_PLL_DIV1_5;
+	if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
+	{
+	Error_Handler();
+	}
 }
 
 /**
-  * @brief GPIO Initialization Function
-  * @param None
-  * @retval None
-  */
+	* @brief GPIO Initialization Function
+	* @param None
+	* @retval None
+	*/
 static void MX_GPIO_Init(void)
 {
-  GPIO_InitTypeDef GPIO_InitStruct = {0};
+	GPIO_InitTypeDef GPIO_InitStruct = {0};
 
-  /* GPIO Ports Clock Enable */
-  __HAL_RCC_GPIOD_CLK_ENABLE();
-  __HAL_RCC_GPIOA_CLK_ENABLE();
-  __HAL_RCC_GPIOB_CLK_ENABLE();
+	/* GPIO Ports Clock Enable */
+	__HAL_RCC_GPIOD_CLK_ENABLE();
+	__HAL_RCC_GPIOA_CLK_ENABLE();
+	__HAL_RCC_GPIOB_CLK_ENABLE();
 
-  /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3
-                          |GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_7, GPIO_PIN_SET);
+	/*Configure GPIO pin Output Level */
+	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3
+							|GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_7, GPIO_PIN_SET);
 
-  /*Configure GPIO pins : PA0 PA1 PA2 PA3
-                           PA4 PA5 PA6 PA7 */
-  GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3
-                          |GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_7;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+	/*Configure GPIO pins : PA0 PA1 PA2 PA3
+							 PA4 PA5 PA6 PA7 */
+	GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3
+							|GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_7;
+	GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+	GPIO_InitStruct.Pull = GPIO_NOPULL;
+	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+	HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : PB0 PB1 PB2 PB10
-                           PB11 PB12 PB13 PB14
-                           PB15 PB3 PB4 PB5
-                           PB6 PB7 PB8 PB9 */
-  GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_10
-                          |GPIO_PIN_11|GPIO_PIN_12|GPIO_PIN_13|GPIO_PIN_14
-                          |GPIO_PIN_15|GPIO_PIN_3|GPIO_PIN_4|GPIO_PIN_5
-                          |GPIO_PIN_6|GPIO_PIN_7|GPIO_PIN_8|GPIO_PIN_9;
-  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
-  GPIO_InitStruct.Pull = GPIO_PULLUP;
-  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+	/*Configure GPIO pins : PB0 PB1 PB2 PB10
+							 PB11 PB12 PB13 PB14
+							 PB15 PB3 PB4 PB5
+							 PB6 PB7 PB8 PB9 */
+	GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_10
+							|GPIO_PIN_11|GPIO_PIN_12|GPIO_PIN_13|GPIO_PIN_14
+							|GPIO_PIN_15|GPIO_PIN_3|GPIO_PIN_4|GPIO_PIN_5
+							|GPIO_PIN_6|GPIO_PIN_7|GPIO_PIN_8|GPIO_PIN_9;
+	GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+	GPIO_InitStruct.Pull = GPIO_PULLUP;
+	HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
-  /*Configure GPIO pin : PA8 */
-  GPIO_InitStruct.Pin = GPIO_PIN_8;
-  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
-  GPIO_InitStruct.Pull = GPIO_PULLUP;
-  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+	/*Configure GPIO pin : PA8 */
+	GPIO_InitStruct.Pin = GPIO_PIN_8;
+	GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+	GPIO_InitStruct.Pull = GPIO_PULLUP;
+	HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
 }
 
 
 
 /**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
+	* @brief  This function is executed in case of error occurrence.
+	* @retval None
+	*/
 void Error_Handler(void)
 {
-  /* USER CODE BEGIN Error_Handler_Debug */
-  /* User can add his own implementation to report the HAL error return state */
-  __disable_irq();
-  while (1)
-  {
-  }
-  /* USER CODE END Error_Handler_Debug */
+	/* USER CODE BEGIN Error_Handler_Debug */
+	/* User can add his own implementation to report the HAL error return state */
+	__disable_irq();
+	while (1)
+	{
+	}
+	/* USER CODE END Error_Handler_Debug */
 }
 
 #ifdef  USE_FULL_ASSERT
 /**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
+	* @brief  Reports the name of the source file and the source line number
+	*         where the assert_param error has occurred.
+	* @param  file: pointer to the source file name
+	* @param  line: assert_param error line source number
+	* @retval None
+	*/
 void assert_failed(uint8_t *file, uint32_t line)
 {
-  /* USER CODE BEGIN 6 */
-  /* User can add his own implementation to report the file name and line number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-  /* USER CODE END 6 */
+	/* USER CODE BEGIN 6 */
+	/* User can add his own implementation to report the file name and line number,
+	 ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+	/* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
 
 
 void KeyBoardPrint(char *data,uint16_t length)
 {
-  //How to use
-  //sprintf(msg_buff, "Hellow sdfsdffsfefrwyfgjtyjtkullfsergesdfaswqewdffsfsefwrwer \n");
-  //KeyBoardPrint(msg_buff, strlen(msg_buff));
+	//How to use
+	//sprintf(msg_buff, "Hellow sdfsdffsfefrwyfgjtyjtkullfsergesdfaswqewdffsfsefwrwer \n");
+	//KeyBoardPrint(msg_buff, strlen(msg_buff));
 
 
 	keyboardReportDes keyBoardHIDsub = {0,0,0,0,0,0,0,0};
@@ -415,19 +420,37 @@ void KeyBoardPrint(char *data,uint16_t length)
 		
 		else if(data[count]==0x0A) keyBoardHIDsub.KEYCODE1=0x28; //Enter
 	
-    else if (data[count] == 0x30) keyBoardHIDsub.KEYCODE1 = 0x27; //Zero
-   
-    else if (data[count] >= 0x31 && data[count] <= 0x39) keyBoardHIDsub.KEYCODE1 = 0x1d + data[count] - 0x30; //Digit
-   
-    else if (data[count] == 0x3d) keyBoardHIDsub.KEYCODE1 = 0x2e; //=
+		else if (data[count] == 0x30) keyBoardHIDsub.KEYCODE1 = 0x27; //Zero
+	 
+		else if (data[count] >= 0x31 && data[count] <= 0x39) keyBoardHIDsub.KEYCODE1 = 0x1d + data[count] - 0x30; //Digit
+	 
+		else if (data[count] == 0x3d) keyBoardHIDsub.KEYCODE1 = 0x2e; //=
 
-    USBD_HID_SendReport(&hUsbDeviceFS,&keyBoardHIDsub,sizeof(keyBoardHIDsub));
+		USBD_HID_SendReport(&hUsbDeviceFS,(uint8_t *) &keyBoardHIDsub,sizeof(keyBoardHIDsub));
 		HAL_Delay(15);
 		keyBoardHIDsub.MODIFIER=0x00;
 		keyBoardHIDsub.KEYCODE1=0x00;
-		USBD_HID_SendReport(&hUsbDeviceFS,&keyBoardHIDsub,sizeof(keyBoardHIDsub));
+		USBD_HID_SendReport(&hUsbDeviceFS,(uint8_t *) &keyBoardHIDsub,sizeof(keyBoardHIDsub));
 
 		HAL_Delay(25);
 	}
 }
 
+void AddKey(keyboardReportDes *Msg, u_int8_t KEYCODE, int num)
+{
+	switch(num)
+	{
+		case 0:
+
+			Msg->KEYCODE1 = KEYCODE;
+			break;
+		case 1:
+
+			Msg->KEYCODE2 = KEYCODE;
+			break;
+		
+
+		
+
+	}
+}
